30fb613df6e431aa460eddaa0c1b7678e5869542
nicolas <ns.seiller@gmail.com>
2020-07-28 14:44:17 +0200

    first operator

--- rpn_calculator_core/stack.py
+++ rpn_calculator_core/stack.py
@@ -1,3 +1,13 @@
+class Operator(object):
+    def __init__(self, cardinality=2):
+        self._cardinality = cardinality
+
+
+class AddOperator(Operator):
+    def execute(self, left_operand, right_operand):
+        return left_operand + right_operand
+
+
 class Stack(object):
     def __init__(self):
         self._stack = []
--- rpn_calculator_tests/test_stack.py
+++ rpn_calculator_tests/test_stack.py
@@ -1,7 +1,14 @@
-from rpn_calculator_core.stack import Stack
+from rpn_calculator_core.stack import Stack, AddOperator
+
 
 def test_push():
     stack = Stack()
     stack.push(1)
     stack.push(2)
     assert stack._stack == [1, 2]
+
+
+def test_execute():
+    operator = AddOperator()
+    assert operator.execute(1, 2) == 3
+
