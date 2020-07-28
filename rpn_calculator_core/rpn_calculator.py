044255d45141bbac81a8889ac0de156a27e8e6b5
nicolas <ns.seiller@gmail.com>
2020-07-28 15:25:18 +0200

    Encapsulate values and execute

---
+++ rpn_calculator_core/rpn_calculator.py
@@ -0,0 +1,38 @@
+class Operator(object):
+    def __init__(self, cardinality=2):
+        self._cardinality = cardinality
+
+
+class AddOperator(Operator):
+    def execute(self, left_operand, right_operand):
+        return left_operand.execute() + right_operand.execute()
+
+
+class Value(Operator):
+    def __init__(self, value):
+        super().__init__(cardinality=0)
+        self._value = value
+
+    def execute(self):
+        return self._value
+
+class Calculator(object):
+    def __init__(self):
+        self._stack = []
+        self._operator_registry = {
+            '+': AddOperator()
+        }
+
+    def push(self, value):
+        try:
+            float(value)
+            self._stack.append(Value(value))
+        except ValueError:
+            if value in self._operator_registry:
+                self._stack.append(self._operator_registry[value])
+
+    def execute(self):
+        operator = self._stack.pop()
+        left_operand = self._stack.pop()
+        right_operand = self._stack.pop()
+        return operator.execute(left_operand, right_operand)
--- rpn_calculator_tests/test_stack.py
+++ rpn_calculator_tests/test_stack.py
@@ -1,14 +1,27 @@
-from rpn_calculator_core.stack import Stack, AddOperator
+from rpn_calculator_core.rpn_calculator import Calculator, AddOperator, Value
+import pytest
 
 
-def test_push():
-    stack = Stack()
-    stack.push(1)
-    stack.push(2)
-    assert stack._stack == [1, 2]
+@pytest.fixture
+def simple_calculator():
+    calculator = Calculator()
+    calculator.push(1)
+    calculator.push(2)
+    return calculator
 
 
-def test_execute():
+def test_push(simple_calculator):
+    assert simple_calculator._stack[0].execute() == 1
+    assert simple_calculator._stack[1].execute() == 2
+
+
+def test_operator():
     operator = AddOperator()
-    assert operator.execute(1, 2) == 3
+    assert operator.execute(Value(1), Value(2)) == 3
+
+
+def test_execute(simple_calculator):
+    simple_calculator.push('+')
+    assert simple_calculator.execute() == 3
+
 
